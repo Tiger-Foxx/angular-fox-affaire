577c5278f5a915c840591b662ae1113fbfe46a20
Tiger-Foxx <donfackarthur750@gmail.com>
2024-12-30 17:35:29 +0100

    Create details component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {ListeComponent} from './liste/liste.component';
+import {DetailComponent} from './detail/detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"",component:ListeComponent},
+  {path:"details",component:DetailComponent}
+];
