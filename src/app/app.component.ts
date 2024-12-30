038568603a5fc3d7a09edb55d5c3657835195c0e
Tiger-Foxx <donfackarthur750@gmail.com>
2024-12-30 12:36:25 +0100

    generate article component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   title = 'angular-fox-affaire';
+  constructor() {
+  }
+  ngOnInit():void {
+    console.log('Initialisation du composant...');
+  }
 }
