eb1788909b26abbd41da1983376054e27a48e73d
Tiger-Foxx <donfackarthur750@gmail.com>
2024-12-30 16:54:02 +0100

    create my first service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ArticleComponent} from './article/article.component';
 import {NgForOf} from '@angular/common';
+import {DataService} from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -17,18 +18,15 @@ export class AppComponent implements  OnInit {
   prix3: number = 2000;
   prix4: number = 8000;
   Message: string;
-  liste=[
-    { id: 1, titre: 'Velo', description: 'Description de l\'article 1', prix: 7000, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0g4tX_4eGcJYoU3cCCwwKbPAVWgrVdHAXTQ&s', likes: 10 ,dispo:true},
-    { id: 2, titre: 'Fox canap', description: 'Description de l\'article 2', prix: 5000, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeu19RC3zwAFhH9NmpIVBYOFH4_tePzx5BIA&s', likes: 5,dispo:true },
-    { id: 3, titre: 'Super TV fox', description: 'Description de l\'article 3', prix: 2000, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyL4GKnYO2vJChXwIbChls_E2xVIxiJmebgg&s', likes: 2,dispo:true },
-    { id: 4, titre: 'Fox Phone', description: 'Description de l\'article 4', prix: 8000, image: 'https://cdn.mos.cms.futurecdn.net/AVEcca7TuDmt8wjaFZPkzj.jpg',dispo: false }
-  ];
+  liste;
 
-  constructor() {
+
+  constructor(private dataService:DataService) {
   }
 
   ngOnInit(): void {
     console.log('Initialisation du composant...');
+    this.liste= this.dataService.listeArticles;
   }
 
   onAffiche(arg: string) {
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,6 +4,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DataService {
-
+  listeArticles=[
+    { id: 1, titre: 'Velo', description: 'Description de l\'article 1', prix: 7000, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0g4tX_4eGcJYoU3cCCwwKbPAVWgrVdHAXTQ&s', likes: 10 ,dispo:true},
+    { id: 2, titre: 'Fox canap', description: 'Description de l\'article 2', prix: 5000, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeu19RC3zwAFhH9NmpIVBYOFH4_tePzx5BIA&s', likes: 5,dispo:true },
+    { id: 3, titre: 'Super TV fox', description: 'Description de l\'article 3', prix: 2000, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyL4GKnYO2vJChXwIbChls_E2xVIxiJmebgg&s', likes: 2,dispo:true },
+    { id: 4, titre: 'Fox Phone', description: 'Description de l\'article 4', prix: 8000, image: 'https://cdn.mos.cms.futurecdn.net/AVEcca7TuDmt8wjaFZPkzj.jpg',dispo: false }
+  ];
   constructor() { }
 }
